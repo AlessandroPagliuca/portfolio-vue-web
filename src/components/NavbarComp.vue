8d11c16f569ddd910e800c12535a3dc100f6fccb
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-09-12 11:55:17 +0200

    Upgraded function hamburger menu

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,11 +1,11 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-3">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" @click="toggleNavbar" aria-expanded="false"
+            aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div :class="['collapse', 'navbar-collapse', { 'show': isNavbarOpen }]" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
@@ -19,16 +19,26 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Projects</a>
                 </li>
-
             </ul>
         </div>
     </nav>
 </template>
-
+  
 <script>
 export default {
     name: 'NavbarComp',
-}
+    data() {
+        return {
+            isNavbarOpen: false, // Variabile di stato per il menu a tendina
+        };
+    },
+    methods: {
+        toggleNavbar() {
+            this.isNavbarOpen = !this.isNavbarOpen; // Inverte lo stato del menu a tendina
+        },
+    },
+};
 </script>
-
+  
 <style lang="scss" scoped></style>
+  
