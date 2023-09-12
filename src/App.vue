fca6efe8b0d7429f9fa8fa2f173d27088221a744
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-09-12 11:18:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <RouterView></RouterView>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeVIew.vue
@@ -0,0 +1,15 @@
+<template>
+    <NavbarComp />
+</template>
+
+<script>
+import NavbarComp from '../components/NavbarComp.vue';
+export default {
+    name: 'HomeView',
+    components: {
+        NavbarComp,
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
