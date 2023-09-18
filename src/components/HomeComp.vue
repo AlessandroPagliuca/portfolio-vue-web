7bc0abd2235cf5dae41319ea52484eb8f4032019
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-09-18 22:52:04 +0200

    testing template position

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -1,6 +1,15 @@
 <template>
-    <div id="home" v-scroll-spy class="container-box container test-cont">
-        <!-- Contenuto della sezione Home -->home
+    <div id="home" class="container-box container">
+        <!-- Contenuto della sezione Home -->
+        <div class="row h-100 test-cont">
+            <div class="col-12 col-md-6 test-cont">
+                left
+            </div>
+            <div class="col-12 col-md-6 test-cont">
+                right
+            </div>
+        </div>
+
     </div>
 </template>
 
