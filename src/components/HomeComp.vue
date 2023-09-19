457b8dd887d13a2613063dff0e406e6eb17781c5
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-09-19 15:23:20 +0200

    fix h1 content

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -4,8 +4,9 @@
         <div class="row h-100">
             <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-start">
                 <p class="display-6 pb-4">Welcome to my web site</p>
-                <h1 class="display-5 fw-bold"> I'm Alessandro Pagliuca a <span>Junior Full Stack web developer</span> from
-                    Italy
+                <h1 class="display-5 fw-bold">
+                    I'm Alessandro a <span>Junior Full Stack web developer</span>
+                    from Italy
                 </h1>
             </div>
             <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
